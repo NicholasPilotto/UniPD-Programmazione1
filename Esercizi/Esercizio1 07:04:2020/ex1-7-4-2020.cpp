28d4f1fd21e22d298d4df2556df9c601be336360
Nicholas Pilotto <nicholaspilotto@gmail.com>
2020-04-08 17:15:57 +0200

    Esercizio1 07/04/2020 con invarianti

--- Esercizi/Esercizio1 07:04:2020/ex1-7-4-2020.cpp
+++ Esercizi/Esercizio1 07:04:2020/ex1-7-4-2020.cpp
@@ -63,6 +63,19 @@ int main() {
     cin >> r;
   }
 
+  // PRE=(regular è una variabile bool inizialmente false)&&(ncorr variabile int>=0)&&(gara variabile struttura corsa)&&(KM kilometri totali della gara)
+  // R=(0<=i<ncorr)
+  // 1) i = 0 => R
+  // 2) R&&(i<ncorr) ->
+  //  - controllo il numero di rilevazioni del corridore di indice i ->
+  //    - se le rilevazioni del corridore indice i < 3
+  //      - il corridore ha usato una scorciatoia, stampo "i ': salto sensore'" e poi regular diventa false
+  //      - altrimenti ->
+  //        - o1 variabile orario contentente la differenza dei tempi rilevati nel primo tratto, o2 variabile orario contentente la differenza dei tempi rilevati nel secondo tratto,
+  //          calcolo il numero di secondi totali di o1 e lo salvo in seconds1, calcolo il numero di secondi totali di o2 e lo salvo in seconds2 ->
+  //            - se seconds1 / KM < 100 oppure seconds2 / KM < 100 ->
+  //              - il corridore ha usato un veicolo, stampo "i ': tempo sospetto'" e poi regular diventa false
+  //  3) i>= ncorr => POST
   bool regular = true;
   for (int i = 0; i < ncorr; i++) {
     if (gara[i].rilevazione < 3) {
@@ -84,11 +97,12 @@ int main() {
   if (regular) {
     cout << "gara regolare" << endl;
   }
-
+  // POST=(sono stati controllati tutti i corridori, se nessuno ha commesso infrazioni è stato stampato "gara regolare", altrimenti sono stati segnalati i corridori e la loro infrazione)
   //controllo dei problemi e stampa in ordine
   return 0;
 }
 
+// PRE=(s è la finestra di lettura dei dati, x è una variabile orario dove verranno salvati i dati ricevuti in lettura da s)
 istream& operator>>(istream& s, orario& x) {
   int h;
   int mim;
@@ -99,7 +113,9 @@ istream& operator>>(istream& s, orario& x) {
   x.secondi = sec;
   return s;
 }
+// POST=(sono stati letti i dati da s e salvati nella struttira orario x)
 
+// PRE=(s è la finestra di lettura dei dati, x è una variabile orario dove verranno salvati i dati ricevuti in lettura da s)
 istream& operator>>(istream& s, rileva& x) {
   int idSens;
   int idPar;
@@ -115,7 +131,9 @@ istream& operator>>(istream& s, rileva& x) {
 
   return s;
 }
+// POST=(sono stati letti i dati da s e salvati nella struttira rileva x)
 
+// PRE=(a, b sono due variabili struttura orario, non vuote)
 orario operator-(orario a, orario b) {
   orario o;
   o.ore = a.ore - b.ore;
@@ -123,7 +141,10 @@ orario operator-(orario a, orario b) {
   o.secondi = a.secondi - b.secondi;
   return o;
 }
+// POST=(è stata effettuata la sottrazione di tutti i campi delle struttute e salvati i risultati in una struttuta orario o)
 
+// PRE=(time è una variabile struttura orario non nulla)
 int hour_to_second(orario time) {
   return (time.ore * 60 * 60) + (time.minuti * 60) + time.secondi;
 }
+// POST=(calcolata la variabile struttura time in secondi)
